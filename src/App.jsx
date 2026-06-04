4f6a18a7106e1ad8dcbb801e7410911954cd735b
Pascal <pascalsolarluchs@gmail.com>
2026-06-03 23:02:45 -0300

    feat: implement complete Importkosten-Kalkulator (React + Vite + Tailwind + Docker)

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import { useKalkulation } from './hooks/useKalkulation.js'
+import { useT } from './i18n/translations.js'
+import Sidebar from './components/layout/Sidebar.jsx'
+import MainArea from './components/layout/MainArea.jsx'
+
+export default function App() {
+  const kalk = useKalkulation()
+  const t = useT(kalk.sprache)
+
+  const handlers = {
+    setProduktWaehrung:     kalk.setProduktWaehrung,
+    setProduktWechselkurs:  kalk.setProduktWechselkurs,
+    updateProduktZeile:     kalk.updateProduktZeile,
+    addProduktZeile:        kalk.addProduktZeile,
+    removeProduktZeile:     kalk.removeProduktZeile,
+    setFleteWaehrung:       kalk.setFleteWaehrung,
+    setFleteWechselkurs:    kalk.setFleteWechselkurs,
+    updateFleteZeile:       kalk.updateFleteZeile,
+    addFleteZeile:          kalk.addFleteZeile,
+    removeFleteZeile:       kalk.removeFleteZeile,
+    updateImportacionZeile: kalk.updateImportacionZeile,
+    addImportacionZeile:    kalk.addImportacionZeile,
+    removeImportacionZeile: kalk.removeImportacionZeile,
+    updateNationaleZeile:   kalk.updateNationaleZeile,
+    addNationaleZeile:      kalk.addNationaleZeile,
+    removeNationaleZeile:   kalk.removeNationaleZeile,
+  }
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar
+        verlauf={kalk.verlauf}
+        aktivId={kalk.aktivId}
+        sprache={kalk.sprache}
+        t={t}
+        onSelect={kalk.setAktivId}
+        onNeu={kalk.neueKalkulation}
+        onUmbenennen={kalk.umbenennen}
+        onLoeschen={kalk.loeschen}
+        onSprache={kalk.setSprache}
+      />
+      <MainArea
+        kalk={kalk.aktiveKalk}
+        t={t}
+        handlers={handlers}
+      />
+    </div>
+  )
+}
